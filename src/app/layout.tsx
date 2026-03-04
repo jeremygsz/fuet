3d68dedce361cdc0a6c33f97e04dead09d800d2a
jeremygsz <jeremygrosz@gmail.com>
2026-03-04 22:18:08 +0100

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
 import type { Metadata } from 'next';
 import './globals.css';
-
 export const metadata: Metadata = {
     title: 'Mariage Agathe & Calvin',
     description: 'Mariage Agathe & Calvin',
-};
 
+    // OBLIGATOIRE pour WhatsApp : l'URL complète de ton site en production
+    metadataBase: new URL('https://fuet.vercel.app/'),
+
+    openGraph: {
+        title: 'Mariage Agathe & Calvin',
+        description: 'Mariage Agathe & Calvin',
+        url: 'https://ton-site.vercel.app',
+        siteName: 'Toutes les infos dont vous avez besoin',
+        locale: 'fr_FR',
+        type: 'website',
+    },
+}
 export default function RootLayout({children,}:{ children: React.ReactNode;})
 {
     return (
